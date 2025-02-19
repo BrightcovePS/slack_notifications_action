35d37b03a5c2c4377261f961650661313d3da839
Stefan <1240279+stfnhh@users.noreply.github.com>
2025-02-19 13:32:07 -0500

    update message

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ async function run() {
         const slackChannel = core.getInput('slack_channel_id');
         const status = core.getInput('status');
         const environment = core.getInput('environment');
+        const name = core.getInput('name');
         const actor = core.getInput('actor');
         const runId = core.getInput('run_id');
         const ts = core.getInput('ts') || null;
@@ -39,28 +40,30 @@ async function run() {
                     color: color,
                     blocks: [
                         {
-                          type: 'section',
-                          text: {
-                            type: 'mrkdwn',
-                            text: '*${{ env.NAME }} deployment started*'
-                          }
-                        },
-                        {
                             type: 'section',
                             fields: [
+                              {
+                                type: 'mrkdwn',
+                                text: `*Component:* ${name}`
+                              },
                               {
                                 type: 'mrkdwn',
                                 text: `*Environment:* ${environment}`
                               },
                               {
                                 type: 'mrkdwn',
-                                text: `*Action* *<${githubRunUrl}|View Action>*`
-                              },
+                                text: `*Triggered by:* ${actor}\n`
+                              }   ,                           
                               {
                                 type: 'mrkdwn',
-                                text: `*Triggered by:* ${actor}\n`
-                              }                              
-                            ]
+                                text: `*<${githubRunUrl}|View Action>*`
+                              }
+                            ],
+                            accessory: {
+                              type: 'image',
+                              image_url: "https://api.slack.com/img/blocks/bkb_template_images/approvalsNewDevice.png",
+                              alt_text: "computer thumbnail"
+                            }
                         }
                     ]
                 }
